3da50217863288422f82b1fe8e732a7075f1ce51
david-wirelab <info@wirelab.co.uk>
2019-05-21 16:24:10 +0100

    output for debugging

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "sns_topic_arn" {
   description = "The ARN of the SNS topic"
   value       = "${aws_sns_topic.default.arn}"
 }
+
+output "role_id" {
+  description = "ID of the IAM role"
+  value = "${aws_iam_role.lambda_role_slack.id}"
+}
+
+output "role_arn" {
+  description = "ARN of the IAM role"
+  value = "${aws_iam_role.lambda_role_slack.arn}"
+}
