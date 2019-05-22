f04de6d07ecf72e577086c4c69999d13c4e61945
david-wirelab <info@wirelab.co.uk>
2019-05-22 10:37:06 +0100

    glue SNS to Lambda inc. tests

--- sns.tf
+++ sns.tf
@@ -22,3 +22,9 @@ resource "aws_sns_topic_policy" "default" {
   arn    = "${aws_sns_topic.default.arn}"
   policy = "${data.aws_iam_policy_document.sns_topic_policy.json}"
 }
+
+resource "aws_sns_topic_subscription" "sns_to_lambda" {
+  topic_arn = "${aws_sns_topic.default.arn}"
+  protocol  = "lambda"
+  endpoint  = "${aws_lambda_function.lambda_slack.arn}"
+}
