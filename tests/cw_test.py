ad1267dd343b2f044ef1df6775487121035decb0
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-12-22 20:20:33 +0000

    Skip requesting account id

--- tests/cw_test.py
+++ tests/cw_test.py
@@ -13,6 +13,7 @@ class TestE2E(unittest.TestCase):
               skip_credentials_validation = true
               skip_requesting_account_id = true
               skip_metadata_api_check = true
+              skip_requesting_account_id = true
             }
 
             module "rds_alarms" {
