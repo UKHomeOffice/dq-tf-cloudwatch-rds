dda8b2da22eb13ef7712442a411557a992241a25
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-12-22 20:22:20 +0000

    Revert "Skip requesting account id"
    This reverts commit ad1267dd343b2f044ef1df6775487121035decb0.

--- tests/cw_test.py
+++ tests/cw_test.py
@@ -13,7 +13,6 @@ class TestE2E(unittest.TestCase):
               skip_credentials_validation = true
               skip_requesting_account_id = true
               skip_metadata_api_check = true
-              skip_requesting_account_id = true
             }
 
             module "rds_alarms" {
