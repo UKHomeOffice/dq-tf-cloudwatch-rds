501d7a9ceda1521e240f170067f8ccd3861de8fc
Gurukishore Bolisetty <gurukishore.bolisetty1@digital.homeoffice.gov.uk>
2025-02-27 10:46:37 +0000

    re-enable life cycle block

--- lambda.tf
+++ lambda.tf
@@ -27,13 +27,13 @@ resource "aws_lambda_function" "lambda_slack" {
 
   depends_on = [aws_iam_role.lambda_role_slack]
 
-  #lifecycle {
-  #  ignore_changes = [
-  #    filename,
-  #    source_code_hash,
-  #    runtime
-  #  ]
-  #}
+  lifecycle {
+    ignore_changes = [
+      filename,
+      source_code_hash,
+      runtime
+    ]
+  }
 
 }
 
